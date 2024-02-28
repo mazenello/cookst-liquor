32e7c704011b91f94a92533ce59b8d6dba84208a
Maz Al Wafai <mazen.wafai@hotmail.com>
2024-02-27 19:40:45 -0800

    wip, doing a diff between the two data sets

--- collectionItems.js
+++ collectionItems.js
@@ -1,6 +1,7 @@
 const https = require('https');
-const collectionId = '6577d832bc98530c12441949'; // Staff picks collection
-const apiToken = '07f757facf042b8064a06ecffe8162e5ff1e2cf5373ac4c18aa12b7fe0d56982'; // Replace with your Webflow API token
+const config = require('./config.js');
+const collectionId = '65dcf5f9a06c08ea96d744bf'; // Staff picks collection
+const apiToken = '9cbf7ce3892a7d2d8f2a4c7e51f6c2f8b01d5637b5be7ff628100fd16efce967'; // Replace with your Webflow API token
 
 function getCollectionItems() {
     return new Promise((resolve, reject) => {
@@ -68,42 +69,42 @@ function deleteItem(itemId, collectionId) {
 function addItem(item, collectionId) {
 
     // Data for the new collection item
-const itemData = JSON.stringify({
-    fields: {
-      name: item.description, // Ensure this matches your collection's requirements
-      _archived: false,
-      _draft: false,
-      slug: item.url
-      // Add other fields as per your collection's design
-    }
-  });
-  
-  const options = {
-    hostname: 'api.webflow.com',
-    path: `/collections/${collectionId}/items`,
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${apiToken}`,
-      'accept-version': '1.0.0',
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(itemData),
-    }
-  };
-  
-  const req = https.request(options, (res) => {
-    console.log(`Status Code: ${res.statusCode}`);
-  
-    res.on('data', (d) => {
-      process.stdout.write(d);
+    const itemData = JSON.stringify({
+        fields: {
+        name: item.description, // Ensure this matches your collection's requirements
+        _archived: false,
+        _draft: false,
+        slug: item.url
+        // Add other fields as per your collection's design
+        }
     });
-  });
   
-  req.on('error', (e) => {
-    console.error(e);
-  });
+    const options = {
+        hostname: 'api.webflow.com',
+        path: `/collections/${collectionId}/items`,
+        method: 'POST',
+        headers: {
+        'Authorization': `Bearer ${apiToken}`,
+        'accept-version': '1.0.0',
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(itemData),
+        }
+    };
   
-  req.write(itemData);
-  req.end();
+    const req = https.request(options, (res) => {
+        console.log(`Status Code: ${res.statusCode}`);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
+  
+    req.on('error', (e) => {
+        console.error(e);
+    });
+
+    req.write(itemData);
+    req.end();
 
 }
 
---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    API_TOKEN : "9cbf7ce3892a7d2d8f2a4c7e51f6c2f8b01d5637b5be7ff628100fd16efce967",
+    BEST_SELLERS_COLLECTION_ID : "65dcf5f9a06c08ea96d744bf",
+    STAFF_PICKS_COLLECTION_ID : "65dcf62e0c63bd43cc91ecaf",
+    BARNETT_IMG_PREFIX : "https://s.barnetnetwork.com/img/t/260/"
+}
+
+module.exports = config;
